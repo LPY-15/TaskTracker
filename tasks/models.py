41cc10764a268370d618ae85d3cf3ffea3142cae
Kirill <befordshir@gmail.com>
2026-04-26 02:13:29 +0300

    added models and requirements.txt

--- tasks/models.py
+++ tasks/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Member(models.Model):
+    first_name = models.CharField(max_length=30, default='unknown')
+    last_name = models.CharField(max_length=30, default='unknown')
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
